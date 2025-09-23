e846e5a01417faf7fc697d75c3c7274a7522eeb7
nandanannn24 <priyogalistyoananda26@gmail.com>
2025-09-24 00:34:10 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startBtn = document.getElementById("startBtn");
 
+  startBtn.addEventListener("click", () => {
+    alert("Game dimulai! (Nanti bisa diarahkan ke halaman role/clue)");
+    // Bisa redirect ke halaman lain, misalnya:
+    // window.location.href = "game.html";
+  });
+});
